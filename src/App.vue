9775aff979fbee21081e9026a6d1ae3cb4b5be95
juergenkaeser <37079505+juergenkaeser@users.noreply.github.com>
2024-08-20 11:30:25 +0200

    adds glide.js slider to ProjectList

--- src/App.vue
+++ src/App.vue
@@ -30,22 +30,6 @@
     <div class="projects" ref="projects">
       <ProjectList />
     </div>
-    
-    <!-- TODO: Make me work!    
-    <div class="glide">
-      <div class="glide__track" data-glide-el="track">
-        <ul class="glide__slides">
-          <li class="glide__slide">0</li>
-          <li class="glide__slide">1</li>
-          <li class="glide__slide">2</li>
-        </ul>
-      </div>
-      <div class="glide__arrows" data-glide-el="controls">
-        <button class="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
-        <button class="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
-      </div>
-    </div> -->
-
   </main>
 </template>
 
--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,12 +1,63 @@
 <script setup lang="ts">
   import ProjectItem from './ProjectItem.vue'
+  import { onMounted } from 'vue'
+  import Glide from '@glidejs/glide'
+
+  onMounted(() => {
+    new Glide('.glide').mount()
+  })
 </script>
 
 <template>
-  <ProjectItem>
-    <template #headline>Headline</template>
-    <template #subline>Subline</template>
-
-    content
-  </ProjectItem>
+  <div class="glide">
+    <div class="glide__track" data-glide-el="track">
+      <ul class="glide__slides">
+        <li class="glide__slide">
+          <ProjectItem>
+            <template #headline>Service Funnel Frontend</template>
+            <template #subline><a href="https://www.o2online.de/service/" target="_blank">Telefónica Germany GmbH & Co. OHG</a></template>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </ProjectItem>
+        </li>
+        <li class="glide__slide">
+          <ProjectItem>
+            <template #headline>Building Frontend Components for a CMS</template>
+            <template #subline><a href="https://www.o2online.de/" target="_blank">Telefónica Germany GmbH & Co. OHG</a></template>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </ProjectItem>
+        </li>
+        <li class="glide__slide">
+          <ProjectItem>
+            <template #headline>Headline 3</template>
+            <template #subline>Subline</template>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </ProjectItem>
+        </li>
+      </ul>
+    </div>
+    <div class="glide__arrows" data-glide-el="controls">
+      <button class="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
+      <button class="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+  @import '@glidejs/glide/dist/css/glide.core.min.css';
+
+  .glide {
+    height: 100%;
+
+    .glide__track {
+      height: 90%;
+
+      .glide__slides {
+        text-align: center;
+      }
+    }
+
+    .glide__arrows {
+      text-align: center;
+    }
+  }
+</style>
